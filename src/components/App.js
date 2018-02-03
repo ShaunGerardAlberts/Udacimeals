dc6fa9c2f79833fc87aff0cc94188e5dc87a03e3
Shaun <alberts.shaun@gmail.com>
2018-02-03 17:20:02 +1000

    feat: Linked up component to store.

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,70 @@
 import React, { Component } from 'react';
-import { addRecipe } from '../actions'
+import { connect } from 'react-redux' // so that we can connect this component to the redux store
+import { addRecipe, removeFromCalendar } from '../actions'
 import logo from '../logo.svg';
 import '../App.css';
 
+// My main component
 class App extends Component {
-  state = {
-    calendar: null
-  }
-
-  componentDidMount() {
-    const { store } = this.props
-     
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }))
-    })
-  }
-
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      meal: 'breakfast',
-      recipe: {
-        label: this.input.value
-      }
-    }))
-
-    this.input.value = ''
+  doThing = () => {
+    //this.props.dispatch(addRecipe({}))
+    this.props.selectRecipe({})
   }
 
   render() {
+    console.log('Props', this.props)
     return (
       <div>
-        <input
-          type='text'
-          ref={(input) => this.input = input}
-          placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-
-        <pre>
-          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
+        Hello World
       </div>
     );
   }
 }
 
-export default App;
+// map the state to the props, so this component has access to the store
+// the store is a object, but we need the props to be a array so work it into an array
+// whatever we return from the function will be mapped to props and usable to the component
+function mapStateToProps(calendar) {
+  //console.log("test")
+  //console.log(calendar)
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  // creates an array called calendar, each array contains an object, and that object has two properties,
+  // day and meal, meal has an object with properties breakfast, lunch, dinner
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        //console.log("!!!!!!!!!!")
+        //console.log("day " + day + " meal " + meal)
+        //console.log(meals[meal])
+        //console.log(calendar[day])
+        meals[meal] = calendar[day][meal] // adding a meal key and setting it to something, then adding it to the meals object
+          ? calendar[day][meal]
+          : null
+
+        //console.log(meals)
+        return meals
+      }, {})
+    })),
+  }
+}
+
+// Dont have to do this, wrapper for the dispatch, dispatch is a wrapper to the action
+function mapDispatchToProps(dispatch) {
+  return {
+    selectRecipe: (data) => { addRecipe(data) },
+    remove: (data) => { removeFromCalendar(data) }
+  }
+}
+
+// connect()(App) - will allow the compont to dispatch actions
+// We want to connect this component to the redux store so that we can get the calendar state thats living inside the redux store
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+
+//meals: Object.keys(calendar[day]).reduce((meals, meal) => {  //want to reduce all of the calendar[day] keys (breakfast, lunch, dinner)
+// to a single object
+
+// meals[meal] = calendar[day][meal] ? calendar[day][meal] : null //meals the object we are building up, current meal of the calendar object
+// breakfast (  ), then lunch then dinner
--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux'
 import reducer from './reducers'
+import { Provider } from 'react-redux'
 
+// from the installed redux library, create the store using the reducer
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+//  log thee store
 console.log(store.getState())
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
-registerServiceWorker();
+// wrap the app component and pass in store here.  npm install --save react-redux (and imported above) makes binding store to components easier
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
